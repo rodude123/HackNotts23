1fea8dbf76d437b245b1f727f9fc265943c6bf57
NIXVOID-1 <richarddejager725@outlook.com>
2023-02-12 12:09:32 +0000

    Full merged versiom

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -2,59 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cannon : MonoBehaviour
+public class Follow : MonoBehaviour
 {
-	public bool running = true;
-	
-	public GameObject ball;
-	public GameObject spawn;
-	public float fireSpeed = 0.9f;
-	private float nextActionTime;
-	
-	private Vector3 baseRotation;
-	public float spinSpeed = 2.5f;
-	public float spinAngle = 70;
-	private float lastSpin;
-	private float nextSpin;
-	private float fullSpin;
-	private float spinProg;
-	
+    public GameObject target;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Shoot();
-		baseRotation = transform.localEulerAngles;
-		lastSpin = Time.time;
-		nextSpin = Time.time + spinSpeed;
-		fullSpin = nextSpin - lastSpin;
-		spinProg = (Time.time - lastSpin)/fullSpin;
+        rb = GetComponent<Rigidbody>();
+        //Physics.IgnoreLayerCollision(0, 6);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Time.time > nextActionTime ) {
-			Shoot();
-			nextActionTime = Time.time + fireSpeed;
-		}
-		if (Time.time > nextSpin ) {
-			lastSpin = Time.time;
-			nextSpin = Time.time + spinSpeed;
-			fullSpin = nextSpin - lastSpin;
-			spinProg = (Time.time - lastSpin)/fullSpin;
-		} else {
-			spinProg = (Time.time - lastSpin)/fullSpin;
-			spinProg = ((spinProg > .5f ? .5f-(spinProg-.5f) : spinProg)*2)-0.5f;
-			Debug.Log(spinProg);
-			transform.localRotation = Quaternion.Euler(baseRotation.x, (spinProg*spinAngle)+baseRotation.y, baseRotation.z);
-		}
+        // Calculate your desired position however you like.
+        Vector3 desiredPosition = target.transform.position;
+
+        // What velocity gets us there in one timestep?
+        Vector3 desiredVelocity = (desiredPosition - transform.position) / Time.deltaTime;
+
+
+        // Apply a sufficient impulse to hit reach that velocity.
+        rb.AddForce(desiredVelocity - rb.velocity, ForceMode.VelocityChange);
+
+        Quaternion diff = Quaternion.Inverse(rb.rotation) * target.transform.rotation;
+        Vector3 eulers = OrientTorque(diff.eulerAngles);
+        Vector3 torque = eulers;
+        //put the torque back in body space
+        torque = rb.rotation * torque;
+
+        //just zero out the current angularVelocity so it doesnt interfere
+        rb.angularVelocity = Vector3.zero;
+
+        rb.AddTorque(torque, ForceMode.VelocityChange);
     }
-	
-	void Shoot()
-	{
-		if(running){
-			GameObject newBall = Instantiate(ball, spawn.transform.position, Quaternion.identity);
-			newBall.GetComponent<Rigidbody>().AddForce(spawn.transform.forward*500);
-		}
-	}
+
+    private Vector3 OrientTorque(Vector3 torque)
+    {
+        // Quaternion's Euler conversion results in (0-360)
+        // For torque, we need -180 to 180.
+
+        return new Vector3
+        (
+        torque.x > 180f ? torque.x - 360f : torque.x,
+        torque.y > 180f ? torque.y - 360f : torque.y,
+        torque.z > 180f ? torque.z - 360f : torque.z
+        );
+    }
+
 }
---
+++ Assets/cannon1.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon1 : MonoBehaviour
+{
+	public bool running = true;
+
+	public float force = 500;
+	
+	public GameObject ball;
+	public GameObject spawn;
+	public float fireSpeed = 0.9f;
+	private float nextActionTime;
+	
+	private Vector3 baseRotation;
+	public float spinSpeed = 2.5f;
+	public float spinAngle = 70;
+	private float lastSpin;
+	private float nextSpin;
+	private float fullSpin;
+	private float spinProg;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot();
+		baseRotation = transform.localEulerAngles;
+		lastSpin = Time.time;
+		nextSpin = Time.time + spinSpeed;
+		fullSpin = nextSpin - lastSpin;
+		spinProg = (Time.time - lastSpin)/fullSpin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime ) {
+			Shoot();
+			nextActionTime = Time.time + fireSpeed;
+		}
+		if (Time.time > nextSpin ) {
+			lastSpin = Time.time;
+			nextSpin = Time.time + spinSpeed;
+			fullSpin = nextSpin - lastSpin;
+			spinProg = (Time.time - lastSpin)/fullSpin;
+		} else {
+			spinProg = (Time.time - lastSpin)/fullSpin;
+			spinProg = ((spinProg > .5f ? .5f-(spinProg-.5f) : spinProg)*2)-0.5f;
+			Debug.Log(spinProg);
+			transform.localRotation = Quaternion.Euler(baseRotation.x, (spinProg*spinAngle)+baseRotation.y, baseRotation.z);
+		}
+    }
+	
+	void Shoot()
+	{
+		if(running){
+			GameObject newBall = Instantiate(ball, spawn.transform.position, Quaternion.identity);
+			newBall.GetComponent<Rigidbody>().AddForce(spawn.transform.forward* force);
+		}
+	}
+}
